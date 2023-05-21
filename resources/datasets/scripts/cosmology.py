67333bc6398b69c05a24ad9142beda20509a1cb4
Alexander Mead <alexander.j.mead@googlemail.com>
2023-05-21 22:26:27 +0100

    new baseline cosmology

--- resources/datasets/scripts/cosmology.py
+++ resources/datasets/scripts/cosmology.py
@@ -31,15 +31,19 @@ kmax_CAMB = 10.*kmax
 seed = 42
 
 # Parameter ranges
-Omega_c_min, Omega_c_max, Omega_c_def = 0.2, 0.3, 0.255
-Omega_b_min, Omega_b_max, Omega_b_def = 0.035, 0.055, 0.045
+Omega_c_min, Omega_c_max, Omega_c_def = 0.15, 0.35, 0.255
+Omega_b_min, Omega_b_max, Omega_b_def = 0.025, 0.065, 0.045
 Omega_k_min, Omega_k_max, Omega_k_def = -0.05, 0.05, 0.
 h_min, h_max, h_def = 0.5, 0.9, 0.7
 ns_min, ns_max, ns_def = 0.91, 1.01, 0.96
-sigma_8_min, sigma_8_max, sigma_8_def = 0.7, 0.9, 0.8
+sigma_8_min, sigma_8_max, sigma_8_def = 0.6, 1.0, 0.8
+As_min, As_max, As_def = 1.1e-9, 3.1e-9, 2.1e-9
 w0_min, w0_max, w0_def = -1.3, -0.7, -1.
 wa_min, wa_max, wa_def = -1.73, 1.28, 0.
 m_nu_min, m_nu_max, m_nu_def = 0., 1., 0.
+
+# Normalization
+norm_sigma_8 = False
 
 # Vary these parameters?
 vary_Omega_c = True
@@ -47,24 +51,32 @@ vary_Omega_b = True
 vary_Omega_k = False
 vary_h = True
 vary_ns = True
-vary_sigma_8 = True
-vary_w0 = False
+vary_sigma_8 = False
+vary_As = False
+vary_w0 = True
 vary_wa = False
 vary_m_nu = False
 
 # Number of cosmologies
 ntrain = 100
-neval = 0
-Latin_sampling = True
+neval = 50
+Latin_sampling = False
 
 # Learning choices
-power_ratio = False
+power_ratio = True
 power_log = False
 
 ### ###
 
 ###  Calculations ###
 
+# Normalisation
+if vary_sigma_8 and vary_As:
+    raise ValueError("Cannot vary both sigma_8 and As")
+if vary_sigma_8 and not norm_sigma_8:
+    raise ValueError("Cannot vary sigma_8 without normalizing by it")
+
+# Filenames
 ratio_bit = '_ratio' if power_ratio else ''
 log_bit = '_log' if power_log else ''
 latin_bit = '_latin' if Latin_sampling else ''
@@ -97,6 +109,7 @@ column_names = [
     "h",
     "ns",
     "sigma_8",
+    "As",
     "w0",
     "wa",
     "m_nu",
@@ -113,12 +126,12 @@ for ival, (n, file) in enumerate(zip([ntrain, neval], [train_file, eval_file])):
 
     # Generate random samples
     if Latin_sampling and ival == 0:
-        sampler = LatinHypercube(d=9, seed=seed, scramble=False)
+        sampler = LatinHypercube(d=10, seed=seed, scramble=False)
         samples = sampler.random(n)
         lower_bounds = [Omega_c_min, Omega_b_min, Omega_k_min,
-                        h_min, ns_min, sigma_8_min, w0_min, wa_min, m_nu_min]
+                        h_min, ns_min, sigma_8_min, As_min, w0_min, wa_min, m_nu_min]
         upper_bounds = [Omega_c_max, Omega_b_max, Omega_k_max,
-                        h_max, ns_max, sigma_8_max, w0_max, wa_max, m_nu_max]
+                        h_max, ns_max, sigma_8_max, As_max, w0_max, wa_max, m_nu_max]
         samples = scale(samples, lower_bounds, upper_bounds)
         Omega_cs = samples[:, 0]
         Omega_bs = samples[:, 1]
@@ -126,9 +139,10 @@ for ival, (n, file) in enumerate(zip([ntrain, neval], [train_file, eval_file])):
         hs = samples[:, 3]
         nss = samples[:, 4]
         sigma_8s = samples[:, 5]
-        w0s = samples[:, 6]
-        was = samples[:, 7]
-        m_nus = samples[:, 8]
+        Ass = samples[:, 6]
+        w0s = samples[:, 7]
+        was = samples[:, 8]
+        m_nus = samples[:, 9]
     else:
         Omega_cs = rng.uniform(Omega_c_min, Omega_c_max, n)
         Omega_bs = rng.uniform(Omega_b_min, Omega_b_max, n)
@@ -136,6 +150,7 @@ for ival, (n, file) in enumerate(zip([ntrain, neval], [train_file, eval_file])):
         hs = rng.uniform(h_min, h_max, n)
         nss = rng.uniform(ns_min, ns_max, n)
         sigma_8s = rng.uniform(sigma_8_min, sigma_8_max, n)
+        Ass = rng.uniform(As_min, As_max, n)
         w0s = rng.uniform(w0_min, w0_max, n)
         was = rng.uniform(wa_min, wa_max, n)
         m_nus = rng.uniform(m_nu_min, m_nu_max, n)
@@ -148,7 +163,7 @@ for ival, (n, file) in enumerate(zip([ntrain, neval], [train_file, eval_file])):
         h = hs[icos] if vary_h else h_def
         ns = nss[icos] if vary_ns else ns_def
         sigma_8 = sigma_8s[icos] if vary_sigma_8 else sigma_8_def
-        As = 2e-9  # Needs to be set and corrected later
+        As = Ass[icos] if vary_As else As_def
         w0 = w0s[icos] if vary_w0 else w0_def
         wa = was[icos] if vary_wa else wa_def
         # while True:  # Ensure that dark energy does not dominate the early universe
@@ -156,17 +171,6 @@ for ival, (n, file) in enumerate(zip([ntrain, neval], [train_file, eval_file])):
         #     if w0+wa < 0.:
         #         break
         m_nu = m_nus[icos] if vary_m_nu else m_nu_def
-
-        # Add cosmological parameters to dataframe
-        for z in zs:
-            params = [z, Omega_c, Omega_b, Omega_k,
-                      h, ns, sigma_8, w0, wa, m_nu]
-            for parameter_name, parameter_value in zip(column_names, params):
-                df[parameter_name].append(parameter_value)
-
-        # Write cosmological parameters to screen
-        print(f"Cosmology: {icos:d}; (Om_c, Om_b, Om_k, h, ns, sig8, w0, wa, m_nu) = \
-({Omega_c:.2}, {Omega_b:.2}, {Omega_k:.2}, {h:.2}, {ns:.2}, {sigma_8:.2}, {w0:.2}, {wa:.2}, {m_nu:.2})")
 
         # CAMB parameters
         H0 = 100.*h           # Hubble constant [km/s/Mpc]
@@ -181,10 +185,24 @@ for ival, (n, file) in enumerate(zip([ntrain, neval], [train_file, eval_file])):
         parameters.InitPower.set_params(As=As, ns=ns)
         parameters.set_matter_power(redshifts=zs, kmax=kmax_CAMB)
         results = camb.get_results(parameters)
-        sigma_8_init = results.get_sigma8_0()
-        As *= (sigma_8/sigma_8_init)**2
-        parameters.InitPower.set_params(As=As, ns=ns)
-        results = camb.get_results(parameters)
+        if norm_sigma_8:
+            sigma_8_init = results.get_sigma8_0()
+            As *= (sigma_8/sigma_8_init)**2
+            parameters.InitPower.set_params(As=As, ns=ns)
+            results = camb.get_results(parameters)
+        else:
+            sigma_8 = results.get_sigma8_0()
+
+        # Write cosmological parameters to screen
+        print(f"Cosmology: {icos:d}; (Om_c, Om_b, Om_k, h, ns, sig8, 10^9 As, w0, wa, m_nu) = \
+({Omega_c:.3}, {Omega_b:.3}, {Omega_k:.3}, {h:.3}, {ns:.3}, {sigma_8:.3}, {As/1e-9:.3}, {w0:.3}, {wa:.3}, {m_nu:.3})")
+
+        # Add cosmological parameters to dataframe
+        for z in zs:
+            params = [z, Omega_c, Omega_b, Omega_k,
+                      h, ns, sigma_8, As, w0, wa, m_nu]
+            for parameter_name, parameter_value in zip(column_names, params):
+                df[parameter_name].append(parameter_value)
 
         # Non-linear power spectrum (from HMcode-2020)
         Pk = hmcode.power(k, zs, results, verbose=False)
